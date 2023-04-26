1f671fe85165be97dd1471779ff25f1bbbcbcf26
michaelayang <pstester2015@gmail.com>
2023-04-25 22:22:53 -0400

    Add files via upload
    Added curl command notes to keplerian_and_ptolemaic_db_load.sql, to load data to postgres tables.

--- AnimateKeplerianPtolemaic/keplerian_and_ptolemaic_db_load.sql
+++ AnimateKeplerianPtolemaic/keplerian_and_ptolemaic_db_load.sql
@@ -2,6 +2,10 @@ cd C:\Program Files\PostgreSQL\15\bin
 psql -h localhost -p 5432 -U postgres
 sudo -u postgres psql
 
+curl --verbose -H "Content-Type:  application/json" -X PUT -d '@keplerian_data_numbered.json' "http://localhost:8080/loadKeplerianRecords"
+curl --verbose -H "Content-Type:  application/json" -X PUT -d '@ptolemaic_data_numbered.json' "http://localhost:8080/loadPtolemaicRecords"
+curl --verbose -H "Content-Type:  application/json" -X PUT -d '@truth_data_numbered.json' "http://localhost:8080/loadTruthDataRecords"
+
 CREATE TABLE PUBLIC.PTOLEMAIC (id BIGINT PRIMARY KEY NOT NULL, first_epicycle_theta REAL NOT NULL, first_epicycle_radius REAL NOT NULL, second_epicycle_theta REAL NOT NULL, second_epicycle_radius REAL NOT NULL, third_epicycle_theta REAL NOT NULL, third_epicycle_radius REAL NOT NULL, ptolemaic_overall_angle REAL NOT NULL);
 
 copy public.ptolemaic from ptolemaic_data_numbered.csv with (FORMAT csv);
